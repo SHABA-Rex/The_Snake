a2e01f0b39a250b4ea8877b63f8b19f27af575de
COLLINS-MBA <83438654+COLLINS-MBA@users.noreply.github.com>
2021-09-22 18:53:09 +0100

    Changement de nom de la fenetre.

--- The_Snake/Form1.Designer.vb
+++ The_Snake/Form1.Designer.vb
@@ -478,7 +478,7 @@ Partial Class Form1
         Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Form1"
+        Me.Text = "The Snake"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
         Me.PerformLayout()
